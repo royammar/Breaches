fc3f5f6f4620102e03e2542c25de1c40b3ae2684
Roy Amar <royammar030783@gmail.com>
2020-07-26 13:38:27 +0300

    fisrt commit

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import './styles/global.scss';
 import BreachPage from './pages/BreachPage'
 import BreachDetails from './pages/BreachDetails'
 import { BrowserRouter, Switch, Route} from 'react-router-dom';
-import { createHashHistory } from 'history';
+import { createBrowserHistory } from 'history';
 import BreachContextProvider from "./context/BreachContext";
 import NavBar from "./cmps/NavBar";
 import Footer from "./cmps/Footer";
 
 
 
-const history = createHashHistory();
+const history = createBrowserHistory();
 function App() {
 
   return (
@@ -18,7 +18,7 @@ function App() {
     <NavBar></NavBar>
       <BreachContextProvider>
         <Switch>
-          <Route path="/breach/" component={BreachPage} exact></Route>
+          <Route path="/breach" component={BreachPage} exact></Route>
           <Route path="/breach/:id" component={BreachDetails} exact></Route>
         </Switch>
       </BreachContextProvider>
