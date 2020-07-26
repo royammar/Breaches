ac097d9fc8db7157843279e27bebdae72210f46b
Roy Amar <royammar030783@gmail.com>
2020-07-26 13:14:59 +0300

    second

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import React from "react";
 import './styles/global.scss';
-import { BrowserRouter, Switch, Route} from 'react-router-dom';
-import { createHashHistory } from 'history';
 import BreachPage from './pages/BreachPage'
 import BreachDetails from './pages/BreachDetails'
+import { BrowserRouter, Switch, Route} from 'react-router-dom';
+import { createHashHistory } from 'history';
 import BreachContextProvider from "./context/BreachContext";
 import NavBar from "./cmps/NavBar";
 import Footer from "./cmps/Footer";
 
 
 
-const history = createHashHistory({
-  hashType: 'slash',
-});
+const history = createHashHistory();
 function App() {
 
   return (
@@ -20,7 +18,7 @@ function App() {
     <NavBar></NavBar>
       <BreachContextProvider>
         <Switch>
-          <Route path="/breach" component={BreachPage} exact></Route>
+          <Route path="/breach/" component={BreachPage} exact></Route>
           <Route path="/breach/:id" component={BreachDetails} exact></Route>
         </Switch>
       </BreachContextProvider>
